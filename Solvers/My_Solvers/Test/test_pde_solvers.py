8438c5060e8b577a0368428229e5a919f5161a48
robrahm <robrahm@tamu.edu>
2025-07-16 15:20:19 -0500

    Added some tests regarding L^2 error in approx

--- Solvers/My_Solvers/Test/test_pde_solvers.py
+++ Solvers/My_Solvers/Test/test_pde_solvers.py
@@ -5,6 +5,7 @@ import numpy as np
 from numpy.testing import assert_allclose
 from My_Solvers.pde_solvers import fdm
 from Pro_Solvers.pro_solvers import solve_heat
+from scipy.interpolate import interp1d
 
 alpha = 2
 L = 1.0
@@ -27,7 +28,13 @@ def test_fd_1():
     T, U = solve_heat(u0, alpha, dx, t_end, L, leftval = 0, rightval = 0)
     assert(np.abs(U1[-1,-1] - U[-1,-1]) <  tol)
     assert(np.abs(U1[-1,0] - U[-1,0]) < tol)
-    assert(dx * np.abs(np.sum(U1[-1]) - np.sum(U[-1])) < dx * np.abs(np.sum(U[-1]) * tol)) 
+    X = np.arange(dx/2, L, dx)
+    inU = interp1d(X, U[-1], kind='cubic', fill_value = "extrapolate")
+    U = inU(X1)
+    diff = dx*np.sum((U1[-1] - U)**2)
+    assert(diff < 1e5 * tol) 
+    
+
 
 #This is dirichlet again, but tests for the case when no BCs are passed which defaults to DD BCs
 def test_fd_1_5():
@@ -35,28 +42,44 @@ def test_fd_1_5():
     T, U = solve_heat(u0, alpha, dx, t_end, L, leftval = 0, rightval = 0)
     assert(np.abs(U1[-1,-1] - U[-1,-1]) <  tol)
     assert(np.abs(U1[-1,0] - U[-1,0]) < tol)
-    assert(dx * np.abs(np.sum(U1[-1]) - np.sum(U[-1])) < dx * np.abs(np.sum(U[-1]) * tol)) 
+    X = np.arange(dx/2, L, dx)
+    inU = interp1d(X, U[-1], kind='cubic', fill_value = "extrapolate")
+    U = inU(X1)
+    diff = dx*np.sum((U1[-1] - U)**2)
+    assert(diff < 1e5 * tol) 
 
 def test_fd_2():
     T1, X1, U1 = fdm(u0, alpha, dx, t_end, L , lbc = [1,0,0], rbc = [0,1,0])
     T, U = solve_heat(u0, alpha, dx, t_end, L, leftval = 0, rightdx = 0)
     assert(np.abs(U1[-1,-1] - U[-1,-1]) <  tol)
     assert(np.abs(U1[-1,0] - U[-1,0]) < tol)
-    assert(dx * np.abs(np.sum(U1[-1]) - np.sum(U[-1])) <  tol)
+    X = np.arange(dx/2, L, dx)
+    inU = interp1d(X, U[-1], kind='cubic', fill_value = "extrapolate")
+    U = inU(X1)
+    diff = dx*np.sum((U1[-1] - U)**2)
+    assert(diff < 1e5 * tol) 
 
 def test_fd_3():
     T1, X1, U1 = fdm(u0, alpha, dx, t_end, L , lbc = [0,1,0], rbc = [0,1,0])
     T, U = solve_heat(u0, alpha, dx, t_end, L, leftdx = 0, rightdx = 0)
     assert(np.abs(U1[-1,-1] - U[-1,-1]) <  tol)
     assert(np.abs(U1[-1,0] - U[-1,0]) < tol)
-    assert(dx * np.abs(np.sum(U1[-1]) - np.sum(U[-1])) < tol)
+    X = np.arange(dx/2, L, dx)
+    inU = interp1d(X, U[-1], kind='cubic', fill_value = "extrapolate")
+    U = inU(X1)
+    diff = dx*np.sum((U1[-1] - U)**2)
+    assert(diff < 1e5 * tol) 
 
 def test_fd_4():
     T1, X1, U1 = fdm(u0, alpha, dx, t_end, L , lbc = [0,1,0], rbc = [1,0,0])
     T, U = solve_heat(u0, alpha, dx, t_end, L, leftdx = 0, rightval = 0)
     assert(np.abs(U1[-1,-1] - U[-1,-1]) <  tol)
     assert(np.abs(U1[-1,0] - U[-1,0]) < tol)
-    assert(dx * np.abs(np.sum(U1[-1]) - np.sum(U[-1])) < tol)
+    X = np.arange(dx/2, L, dx)
+    inU = interp1d(X, U[-1], kind='cubic', fill_value = "extrapolate")
+    U = inU(X1)
+    diff = dx*np.sum((U1[-1] - U)**2)
+    assert(diff < 1e5 * tol) 
 
 
 
@@ -67,25 +90,41 @@ def test_fd_1_nh():
     T, U = solve_heat(u0, alpha, dx, t_end, L, leftval = 0, rightval = 0, g = lambda x,t: (t**2)*np.cos(x))
     assert(np.abs(U1[-1,-1] - U[-1,-1]) <  tol)
     assert(np.abs(U1[-1,0] - U[-1,0]) < tol)
-    assert(dx * np.abs(np.sum(U1[-1]) - np.sum(U[-1])) <  tol)
+    X = np.arange(dx/2, L, dx)
+    inU = interp1d(X, U[-1], kind='cubic', fill_value = "extrapolate")
+    U = inU(X1)
+    diff = dx*np.sum((U1[-1] - U)**2)
+    assert(diff < 1e5 * tol) 
 
 def test_fd_2_nh():
     T1, X1, U1 = fdm(u0, alpha, dx, t_end, L , lbc = [1,0,0], rbc = [0,1,0], g = lambda x,t: (t**2)*np.cos(x))
     T, U = solve_heat(u0, alpha, dx, t_end, L, leftval = 0, rightdx = 0, g = lambda x,t: (t**2)*np.cos(x))
     assert(np.abs(U1[-1,-1] - U[-1,-1]) <  tol)
     assert(np.abs(U1[-1,0] - U[-1,0]) < tol)
-    assert(dx * np.abs(np.sum(U1[-1]) - np.sum(U[-1])) <  tol) 
+    X = np.arange(dx/2, L, dx)
+    inU = interp1d(X, U[-1], kind='cubic', fill_value = "extrapolate")
+    U = inU(X1)
+    diff = dx*np.sum((U1[-1] - U)**2)
+    assert(diff < 1e5 * tol) 
 
 def test_fd_3_nh():
     T1, X1, U1 = fdm(u0, alpha, dx, t_end, L , lbc = [0,1,0], rbc = [0,1,0], g = lambda x,t: (t**2)*np.cos(x))
     T, U = solve_heat(u0, alpha, dx, t_end, L, leftdx = 0, rightdx = 0, g = lambda x,t: (t**2)*np.cos(x))
     assert(np.abs(U1[-1,-1] - U[-1,-1]) <  tol)
     assert(np.abs(U1[-1,0] - U[-1,0]) < tol)
-    assert(dx * np.abs(np.sum(U1[-1]) - np.sum(U[-1])) <  tol)
+    X = np.arange(dx/2, L, dx)
+    inU = interp1d(X, U[-1], kind='cubic', fill_value = "extrapolate")
+    U = inU(X1)
+    diff = dx*np.sum((U1[-1] - U)**2)
+    assert(diff < 1e5 * tol) 
 
 def test_fd_4_nh():
     T1, X1, U1 = fdm(u0, alpha, dx, t_end, L , lbc = [0,1,0], rbc = [1,0,0], g = lambda x,t: (t**2)*np.cos(x))
     T, U = solve_heat(u0, alpha, dx, t_end, L, leftdx = 0, rightval = 0, g = lambda x,t: (t**2)*np.cos(x))
     assert(np.abs(U1[-1,-1] - U[-1,-1]) <  tol)
     assert(np.abs(U1[-1,0] - U[-1,0]) < tol)
-    assert(dx * np.abs(np.sum(U1[-1]) - np.sum(U[-1])) <  tol)
+    X = np.arange(dx/2, L, dx)
+    inU = interp1d(X, U[-1], kind='cubic', fill_value = "extrapolate")
+    U = inU(X1)
+    diff = dx*np.sum((U1[-1] - U)**2)
+    assert(diff < 1e5 * tol) 
